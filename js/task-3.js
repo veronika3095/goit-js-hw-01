5a8b98036772d767714b396502a6e2897e397506
USR <peremei35@gmail.com>
2024-07-10 18:45:47 +0300

    add index.html file

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,11 @@
 function getElementWidth(content, padding, border) { 
-    return content + padding + border;
+
+    const contentWidth = parseFloat (content);
+    const paddingWidth = parseFloat (padding) * 2;
+    const borderWidth = parseFloat(border) * 2;
+
+    const totalWidth = contentWidth + paddingWidth + borderWidth;
+    return totalWidth;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
